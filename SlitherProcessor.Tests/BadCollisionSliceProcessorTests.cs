826607261fbc3e48c130afe68ae68d499ca88809
sharknice <glowystarman@yahoo.com>
2020-05-09 18:17:59 -0500

    collision service

--- SlitherProcessor.Tests/BadCollisionSliceProcessorTests.cs
+++ SlitherProcessor.Tests/BadCollisionSliceProcessorTests.cs
@@ -1,4 +1,7 @@
-﻿using SlitherModel.Source;
+﻿using SlitherModel.Processed.BadCollision;
+using SlitherModel.Source;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace SlitherProcessor.Tests
@@ -19,17 +22,24 @@ namespace SlitherProcessor.Tests
         [Fact]
         void EnemySnakeHead()
         {
+            _slitherFrame.Snakes = new List<Snake>();
+            _slitherFrame.Snakes.Add(new Snake { Xx = 100, Yy = 0, Sc = 2, Pts = new List<Lnp>(), DeadAmt = 0 });
+            _slitherFrame.Snakes.Add(new Snake { Xx = 150, Yy = 0, Sc = 2, Pts = new List<Lnp>(), DeadAmt = 0 });
+            _slitherFrame.Snakes.Add(new Snake { Xx = -100, Yy = 0, Sc = 2, Pts = new List<Lnp>(), DeadAmt = 0 });
             var slice = _badCollisionSliceProcessor.ProcessSlice(_slitherFrame, 0, .1, 10);
-
-            Assert.Equal(8, slice.Count);
+            var snakeCollisions = slice.Where(c => c.GetType() == typeof(SnakeHead));
+            Assert.Equal(2, snakeCollisions.Count());
         }
 
         [Fact]
         void EnemySnakeBody()
         {
-            var slice = _badCollisionSliceProcessor.ProcessSlice(_slitherFrame, 0, .1, 10);
+            _slitherFrame.Snakes = new List<Snake>();
+            _slitherFrame.Snakes.Add(new Snake { Xx = 100, Yy = 0, Sc = 2, Pts = new List<Lnp> { new Lnp { Dying = false, Xx = 50, Yy = 0 }, new Lnp { Dying = false, Xx = 100, Yy = 0 }, new Lnp { Dying = false, Xx = 150, Yy = 0 }, new Lnp { Dying = false, Xx = -150, Yy = 0 } }, DeadAmt = 0 });
 
-            Assert.Equal(8, slice.Count);
+            var slice = _badCollisionSliceProcessor.ProcessSlice(_slitherFrame, 0, .1, 10);
+            var bodyCollisions = slice.Where(c => c.GetType() == typeof(SnakeBody));
+            Assert.Equal(3, bodyCollisions.Count());
         }
 
         [Fact]
--- SlitherProcessor/CollisionService.cs
+++ SlitherProcessor/CollisionService.cs
@@ -6,7 +6,73 @@ namespace SlitherProcessor
     {
         public double? GetDistance(double xx, double yy, double radius, double angleStart, double angleEnd, int distanceStep)
         {
+            var targetAngle = Math.Atan2(yy, xx);
+            if(TargetWithinSlice(targetAngle, angleStart, angleEnd))
+            {
+                return GetDistance(xx, yy, radius, distanceStep);
+            }
+
+            // TODO: checking collision for the point with radius of circle here, probably not the right way to do it
+            targetAngle = Math.Atan2(yy, xx + radius);
+            if (TargetWithinSlice(targetAngle, angleStart, angleEnd))
+            {
+                return GetDistance(xx + radius, yy, radius, distanceStep);
+            }
+
+            targetAngle = Math.Atan2(yy, xx - radius);
+            if(TargetWithinSlice(targetAngle, angleStart, angleEnd))
+            {
+                return GetDistance(xx - radius, yy, radius, distanceStep);
+            }
+
+            targetAngle = Math.Atan2(yy + radius, xx);
+            if(TargetWithinSlice(targetAngle, angleStart, angleEnd))
+            {
+                return GetDistance(xx, yy + radius, radius, distanceStep);
+            }
+
+            targetAngle = Math.Atan2(yy + radius, xx + radius);
+            if (TargetWithinSlice(targetAngle, angleStart, angleEnd))
+            {
+                return GetDistance(xx + radius, yy + radius, radius, distanceStep);
+            } 
+
+            targetAngle = Math.Atan2(yy + radius, xx - radius);
+            if (TargetWithinSlice(targetAngle, angleStart, angleEnd))
+            {
+                return GetDistance(xx - radius, yy + radius, radius, distanceStep);
+            }
+
+            targetAngle = Math.Atan2(yy - radius, xx);
+            if (TargetWithinSlice(targetAngle, angleStart, angleEnd))
+            {
+                return GetDistance(xx, yy - radius, radius, distanceStep);
+            }
+
+            targetAngle = Math.Atan2(yy - radius, xx + radius);
+            if (TargetWithinSlice(targetAngle, angleStart, angleEnd))
+            {
+                return GetDistance(xx + radius, yy - radius, radius, distanceStep);
+            }
+
+            targetAngle = Math.Atan2(yy - radius, xx - radius);
+            if (TargetWithinSlice(targetAngle, angleStart, angleEnd))
+            {
+                return GetDistance(xx - radius, yy - radius, radius, distanceStep);
+            }
+
             return null;
+        }
+
+        bool TargetWithinSlice(double targetAngle, double angleStart, double angleEnd)
+        {
+            return angleStart <= targetAngle && targetAngle <= angleEnd;
+        }
+
+        double GetDistance(double x, double y, double radius, int distanceStep)
+        {
+            var distance = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) - radius;
+            return distance; // TODO: use distanceStep to round
         }
     }
 }
