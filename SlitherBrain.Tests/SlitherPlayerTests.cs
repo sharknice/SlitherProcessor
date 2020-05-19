603e0f592f6362005bd8ed6664cebe427ffa76bd
sharknice <glowystarman@yahoo.com>
2020-05-19 08:18:48 -0500

    stuff

---
+++ SlitherBrain.Tests/SlitherPlayerTests.cs
@@ -0,0 +1,49 @@
+using Newtonsoft.Json;
+using SlitherDatabase;
+using SlitherModel.Source;
+using SlitherProcessor;
+using System.IO;
+using Xunit;
+
+namespace SlitherBrain.Tests
+{
+    public class SlitherPlayerTests
+    {
+        private readonly SlitherPlayer SlitherPlayer;
+        private readonly Game SourceGame;
+
+        public SlitherPlayerTests()
+        {
+            GameDatabase.DatabaseFolder = "C:\\SlitherShark\\test\\processed";
+            GameDatabase.LoadGames();
+
+            var jsonString = File.ReadAllText("C:\\SlitherShark\\test\\source\\test.json");
+            SourceGame = JsonConvert.DeserializeObject<Game>(jsonString, new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Auto,
+                NullValueHandling = NullValueHandling.Ignore,
+            });
+
+            SlitherPlayer = new SlitherPlayer(new FrameProcessor(new OutcomeProcessor(new OutcomeScoreProcessor()), new CollisionMapProcessor(new CollisionMapResolutionProcessor(new CollisionSliceProcessor(new FoodSliceProcessor(new CollisionService()), new BadCollisionSliceProcessor(new CollisionService()), new SelfSliceProcessor(new CollisionService()))), new SlitherFrameNormalizer())));
+        }
+
+        [Fact]
+        public void OutCome()
+        {
+            var frame = SourceGame.Frames[5];
+            var matchingFrame = GameDatabase.Games[0].Frames[5];
+
+            var result = SlitherPlayer.PlayGame("test", frame, 250);
+            Assert.Equal(matchingFrame.Outcome, result.PredictedOutCome);
+        }
+
+        [Fact]
+        public void Match()
+        {
+            var frame = SourceGame.Frames[5];
+
+            var result = SlitherPlayer.PlayGame("test", frame, 250);
+            Assert.Equal(1, result.MatchConfidence);
+        }
+    }
+}
--- SlitherBrain/SlitherPlayer.cs
+++ SlitherBrain/SlitherPlayer.cs
@@ -19,6 +19,13 @@ namespace SlitherBrain
 
             //GameDatabase.Games
 
+            // loop through every frame in the database
+            // first look at the outcome score, if it is negative, skip this frame
+            // calculate how close the frame matches
+            // weight outcome score parameters versus match closeness
+
+            // alive most important, then kills, then growth, but need to decide how much
+
             // TODO: find the best matched frame combined with the best result, then get the next frame from that point based on frameTime, make new project for that processor
             // decision has the angle to go (need to adjust based on the original angle and the processed, normalized angle), decision also has the OutCome from the best match and how close the match is to this game
 
